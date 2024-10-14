e28a7cb0175120e54e22373a583c350571ca1944
SalesTax <masseyas@mail.uc.edu>
2024-10-13 23:00:23 -0400

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
-public class Task7 {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("* ");
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int rollCount = 0;
+        boolean triple = false;
+
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s\n", "Roll", "Die1", "Die2", "Die3", "Sum");
+        System.out.println("----------------------------------------------------------");
+
+        while (!triple) {
+            rollCount++;
+            int die1 = rand.nextInt(6) + 1;
+            int die2 = rand.nextInt(6) + 1;
+            int die3 = rand.nextInt(6) + 1;
+            int sum = die1 + die2 + die3;
+
+            System.out.printf("%-10d%-10d%-10d%-10d%-10d\n", rollCount, die1, die2, die3, sum);
+
+            if (die1 == die2 && die2 == die3) {
+                triple = true;
             }
-            System.out.println();
+
+            System.out.print("Do you want to continue rolling? (yes/no): ");
+            if (!sc.nextLine().equalsIgnoreCase("yes")) {
+                break;
+            }
         }
+        sc.close();
     }
 }
