cb7c690b7330f0811b15ad79ce6ea9808e1dccce
SalesTax <masseyas@mail.uc.edu>
2024-10-13 22:51:32 -0400

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 18; i += 3) {
+        for (int i = 10; i >= 0; i -= 2) {
             System.out.print(i + " ");
         }
     }
